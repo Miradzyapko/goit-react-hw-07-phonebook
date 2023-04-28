25cb1f8ee34d8213159ca9b978c8dbf141faf3c3
Miradzyapko <mira.dzyapko@gmail.com>
2023-04-29 02:53:06 +0300

    fix bug

--- src/redux/action.js
+++ src/redux/action.js
@@ -1,33 +1,5 @@
-/*import { nanoid } from 'nanoid';
-const addContact = ({ name, number }) => {
-    const contacts = {
-      id: nanoid(),
-      name,
-      number,
-    };
 
-    const checkContact = contacts.find((contact) => contact.name.toLowerCase() === name.toLowerCase());
-    if (checkContact) {
-      alert(`${name} is already in contacts.`);
-      return;
-    }
 
-    setContacts(prevState => 
-       [contact, ...prevState]
-    );
-
-  }
-  const handleDeleteContact = id => {
-    setContacts(prevState => 
-      prevState.filter(el => el.id !== id),
-    );
-  };
-  export const setFilter = value => {
-    return {
-      type: "filter/setFilter",
-      payload: value,
-    };
-  };*/
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from 'axios';
 
